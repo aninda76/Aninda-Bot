879914a0dd92625533bece6c4a1560e93efbed7b
aninda76 <164429136+aninda76@users.noreply.github.com>
2024-03-22 05:54:08 +0600

    Update restart.js

--- scripts/commands/restart.js
+++ scripts/commands/restart.js
@@ -1,34 +1,23 @@
 module.exports.config = {
-  name: "restart",
-  version: "1.0.0",
-  permssion: 3,
-  prefix: true,
-  premium: false,
-  credits: "Hamim",
-  description: "Restart the Bot",
-  category: "system",
-  usages: "",
-  cooldowns: 5
+	name: "restart",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "manhIT",
+	description: "Restart the Bot",
+	commandCategory: "system",
+	usages: "",
+	cooldowns: 5
 };
 
 module.exports.run = async ({ api, event, args }) => {
-  const { threadID, messageID } = event;
-  return api.sendMessage(`╭──────────────➣
-│么𝗛𝗔𝗠𝗜𝗠 𝗦𝗘𝗥𝗩𝗘𝗥 is
-│Trying To Restart☢️
-│━━━━•💠•━━━━┓
-│❖Restarting: in 6's
-│   ██████╗
-│   ██         ██╗
-│   ██████╔╝
-│   ██╔══██╗
-│   ██║	    ██║
-│   ╚═╝ 	    ╚═╝
-│	     Restarted..
-│			 🄱🄾🅃
-│  Bot by Hamim
-│━━━━•💠•━━━━┛
-│么 𝗛𝗔𝗠𝗜𝗠 𝗦𝗘𝗥𝗩𝗘𝗥 is
-│Successfully Restarted
-╰──────────────➣`, threadID, () => process.exit(1));
+	const { threadID, messageID } = event;
+	return api.sendMessage(`╭───────😇
+│Aninda BOT Aɪ 🅅-1.9 is 
+│🤖RESTARTING...
+│=============
+│么System RESTART Successful🥳
+│么Processing time: 4s
+│===============
+│BOT CREATOR GOD ANINDA💫🫶
+╰───────😇`, threadID, () => process.exit(1));
 }
